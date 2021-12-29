f2cb74c1016f6c71f82551dc8d7a295f5e38f4dd
fakhariamir <fakhariamir24@gmail.com>
2021-12-29 14:55:44 +0330

    bakhshe panj

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,8 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(upload_to='featured_image/%Y/%m/%d/')  # this
+
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.post_list,name="post_list"),
+    path('hello/', views.hello,name='hello'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
